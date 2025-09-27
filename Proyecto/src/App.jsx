9b64c055b63b10fd6e6ea89ef0b447283d413e6c
jacobogaviria <jacobogav15@gmail.com>
2025-09-27 14:54:51 -0500

    inicio

---
+++ Proyecto/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { Modulos } from './Modulos.jsx'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Modulos />
+    </>
+  )
+}
+
+export default App
---
+++ Proyecto/src/Modulos.jsx
@@ -0,0 +1,39 @@
+import './Modulos.css'
+
+
+export function Modulos() {
+  return (
+    <div className="Modulos">
+        <div className='modulo'>
+            <h2>Modulo 1</h2>
+            <p>Descripción del Módulo 1</p>
+            <a className="modulo-link" href="#">Ir al Módulo 1</a>
+        </div>
+        <div className='modulo'>
+            <h2>Modulo 2</h2>
+            <p>Descripción del Módulo 2</p>
+            <a className="modulo-link" href="#">Ir al Módulo 2</a>
+        </div>
+        <div className='modulo'>
+            <h2>Modulo 3</h2>
+            <p>Descripción del Módulo 3</p>
+            <a className="modulo-link" href="#">Ir al Módulo 3</a>
+        </div>
+        <div className='modulo'>
+            <h2>Modulo 4</h2>
+            <p>Descripción del Módulo 4</p>
+            <a className="modulo-link" href="#">Ir al Módulo 4</a>
+        </div>
+        <div className='modulo'>
+            <h2>Modulo 5</h2>
+            <p>Descripción del Módulo 5</p>
+            <a className="modulo-link" href="#">Ir al Módulo 5</a>
+        </div>
+        <div className='modulo'>
+            <h2>Modulo 6</h2>
+            <p>Descripción del Módulo 6</p>
+            <a className="modulo-link" href="#">Ir al Módulo 6</a>
+        </div>
+    </div>
+  )
+}
